4c225951ac6bb72dd8b5ae37831a8761cda51acb
Kawandeep Virdee <whichlight@gmail.com>
2016-07-10 13:43:29 -0400

    focusing on drawing tests

--- sketch.js
+++ sketch.js
@@ -2,10 +2,14 @@
 var h;
 var w;
 
-var up = false;
 
-var p= {"x":0, "y":0};
+var past;
+var now;
 var col;
+var newgesture = true;
+var endgesture = true;
+
+
 
 var setup = function(){
   colorMode(HSB, 360,1,1)
@@ -24,8 +28,9 @@ var setup = function(){
   el.addEventListener("touchleave", pdefault, false);
   el.addEventListener("touchmove", pdefault, false);
 
+  past = createVector(0, 0);
   background(color(0,0,0));
-  initCover();
+ // initCover();
 }
 
 function windowResized() {
@@ -37,63 +42,115 @@ function pdefault(e){
 }
 
 var touchStarted= function(){
-    p.x = touchX;
-    p.y = touchY;
-
-  pressed(touchX,touchY);
+    newgesture = true;
+    return false;
 }
 
 var touchMoved= function(){
   pressed(touchX,touchY);
+  return false;
 }
 
 var touchEnded= function(){
   pressed(touchX,touchY);
-}
-
-var calcDist = function(past,now){
-  return sqrt(pow(past.x-now.x,2) + pow(past.y - now.y,2));
-}
-
-
-var pressed= function(x,y){
-  drawCircle(x,y);
+  endgesture = true;
+  return false;
 }
 
 var initCover = function(){
   var num = 1000;
-
   for(var i =0; i<num;i++){
     col=color(0,0,floor(random(0,2)));
     ellipse(random(w),random(h),50,50);
     fill(col);
     stroke(col);
   }
-
 }
 
-var drawCircle = function(x,y){
-    var diff = y - p.y;
+var pressed= function(x,y){
+  now = createVector(x,y);
+  if(newgesture){
+    past = now;
+    past.p1 = createVector(0,0);
+    past.p2 = createVector(0,0);
+    newgesture = false;
+    background(0,0,0);
+  }
+  drawCircle(now);
+}
+
+
+var drawCircle = function(now){
+    var diff = now.y - past.y;
     if((diff) > 0){
-      up=false;
       col = color(0,0,1);
     } else if (diff <0){
-      up = true;
       col = color(0,0,0);
     }
 
+      col = color(0,0,1);
+    /*
+    var diffv = p5.Vector.sub(now,past);
+    var ang = atan(diffv.y, diffv.x);
+    ang = (1+(ang/90))/2;
+    console.log(ang);
+    col = color(0,0,ang);
+    */
 
-    var dist = calcDist(p,{"x":x, "y":y});
-    var r = map(sqrt(min(abs(dist),100)),0,5,5,100);
-    ellipse(x,y,r,r);
+    var dist = now.dist(past);
+    var r = map(sqrt(min(dist,100)),0,5,5,50);
+    //ellipse(now.x,now.y,r,r);
+
+    line(now.x,now.y,past.x, past.y)
+    noFill();
+    stroke(col);
+    strokeWeight(2);
+
+    ellipse(now.x,now.y,r,r);
+    stroke(col);
     fill(col);
+
+    //perpendicular
+    var diffv = p5.Vector.sub(now,past);
+    var n1 = createVector(-1*diffv.y, diffv.x);
+    var n2 = createVector(diffv.y, -1*diffv.x);
+
+    var p1 = p5.Vector.add(now,n1.normalize().mult(r));
+    var p2 = p5.Vector.add(now,n2.normalize().mult(r));
+    now.p1 = p1;
+    now.p2 = p2;
+
+    /*
+    line(p1.x,p1.y,p2.x, p2.y)
+    noFill();
     stroke(col);
 
-    p.x = x;
-    p.y = y;
+    //shape
+    noFill();
+    stroke(col);
+
+
+    beginShape();
+    vertex(past.p1.x,past.p1.y);
+    vertex(past.p2.x,past.p2.y);
+
+    vertex(now.p2.x,now.p2.y);
+    vertex(now.p1.x,now.p1.y);
+    endShape();
+
+
+    */
+
+    //shift in time
+    past = now;
 }
 
 var draw = function(){
-
+  if(touchIsDown || mouseIsPressed){
+    if(endgesture){
+      newgesture = true;
+      endgesture = false;
+    }
+  }
 }
 
